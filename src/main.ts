66fafa34b9547c57e8a20961a135e87d25369d6f
Leandro Moura <leandro.moura@N0137347.local>
2024-03-05 14:05:36 -0300

    enable headers cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const PORT = parseInt(process.env.APP_PORT, 10) || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: '*' });
+  app.enableCors({ origin: '*', allowedHeaders: '*', methods: '*' });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
